8e9bfd16e0910347290a59a863fdbf646ab92429
Tony <tursunoteshov@gmail.com>
2025-05-29 17:36:23 +0500

    Delete ,Get User

--- BookingSystem/src/BookingSystem.API/BookingSystem.API/Controllers/UserController.cs
+++ BookingSystem/src/BookingSystem.API/BookingSystem.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using BookingSystem.Application.DTOs.User;
 using BookingSystem.Application.Feature.User.Command;
+using BookingSystem.Application.Feature.User.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -27,6 +28,22 @@ public class UserController : Controller
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
         var result  = await _mediator.Send(new LoginUserCommand(loginUserModel));
+        return Ok(result);
+    }
+    [HttpGet("GetUserById")]
+    public async Task<IActionResult> GetUserById( Guid id)
+    {
+        if(!ModelState.IsValid)
+            return NotFound();
+        var result = await _mediator.Send(new GetUserByIdQueries(id));
+        return Ok(result);
+    }
+    [HttpDelete("DeleteUser")]
+    public async Task<IActionResult> DeleteUser(LoginUserModel id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        var result = await _mediator.Send(new DeleteUserCommand(id));
         return Ok(result);
     }
     public IActionResult Index()
